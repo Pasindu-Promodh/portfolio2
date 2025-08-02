0b12aee5c032bce5c30538f835ab89e0a0395cfb
Pasindu-Promodh <pasindugunathilaka96@gmail.com>
2025-08-02 08:55:53 +0530

    fire

--- src/Analytics/firebase.ts
+++ src/Analytics/firebase.ts
@@ -8,13 +8,20 @@ import { getFunctions } from "firebase/functions";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  // apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  // authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  // projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  // storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  // measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCrE7dagCcQD77lopPMogtyFgGPexQl4_8",
+  authDomain: "portfolio-3431b.firebaseapp.com",
+  projectId: "portfolio-3431b",
+  storageBucket: "portfolio-3431b.firebasestorage.app",
+  messagingSenderId: "1030481561592",
+  appId: "1:1030481561592:web:d3b83347e89110c55f380d",
+  measurementId: "G-QF1L8QFVR2"
 };
 
 // Initialize Firebase
